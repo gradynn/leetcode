9b749e4890ca20aabaec981efbd2a9d877e9eaca
Gradyn Nagle <55261229+gradynn@users.noreply.github.com>
2024-04-30 15:04:25 -0700

    Time: 487 ms (91.84%) | Memory: 23.6 MB (63.81%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,19 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        holding = [-(prices[0] + fee)]
-        notHolding = [0]
+        holding = -(prices[0] + fee)
+        notHolding = 0
 
         for i in range(1, len(prices)):
-            # holding at the end of ith day
-            holding.append(max(
-                holding[i - 1], # we are still holding
-                -(prices[i] + fee) + notHolding[i - 1] # we have bought
-            ))
-
-            # not holding at the end of ith day
-            notHolding.append(max(
-                holding[i - 1] + prices[i], # we have sold
-                notHolding[i - 1], # we are still waiting to buy
-            ))
+            holding, notHolding = max( # holding at end of ith day
+                holding, # we are still holding
+                -(prices[i] + fee) + notHolding # we have bought
+            ), max( # not holding at end of ith day
+                holding + prices[i], # we have sold
+                notHolding # we are still waiting to buy
+            )
         
-        return max(holding[len(prices) - 1], notHolding[len(prices) - 1])
+        return max(holding, notHolding)
