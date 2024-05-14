77577a238a01d30d50eb2b2752d42f3fcfc01fde
Gradyn Nagle <55261229+gradynn@users.noreply.github.com>
2024-05-14 13:56:18 -0700

    Time: 168 ms (70.30%) | Memory: 18 MB (35.43%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,12 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        map = {}
-        for num in nums:
-            if num not in map.keys():
-                map[num] = 1
-            else:
-                map[num] = map[num] + 1
-        
-        for num in set(nums):
-            if map[num] > len(nums) // 2:
-                return num
+        count, res = 0, 0
+
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
+
+        return res
