945ccd0f9c23e3c59acce3faa05a75fa7b39f5f3
Gradyn Nagle <55261229+gradynn@users.noreply.github.com>
2024-05-21 13:33:57 -0700

    Time: 152 ms (82.58%) | Memory: 27.9 MB (91.05%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,10 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums_cpy = [0] * len(nums)
-        for i in range(len(nums)):
-            new_index = (i + k) % len(nums)
-            nums_cpy[new_index] = nums[i]
-
-        for i in range(len(nums)):
-            nums[i] = nums_cpy[i]
+        k = k % len(nums)
+        nums[:] = reversed(nums[:])
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
